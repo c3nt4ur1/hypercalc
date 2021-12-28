ba6d4dfdb28d9a553e15bc380a93956562c3b896
Adriano Naime Bonin <adriano.bonin@outlook.com>
2021-12-28 14:08:08 -0300

    Declared the first degree equation function

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Main function will only be used for testing the functions
+
+    return 0;
+}
+
+double first_deg_equation(string source){
+
+}
